acd43bf7b1b8b8b9594d165b6337232c13012b70
Burrito <burritoyangster@gmail.com>
2025-03-14 19:15:16 +0800

    Fix adding time scale events (#4)

--- src/state/mutations/values/bpm.ts
+++ src/state/mutations/values/bpm.ts
@@ -1,13 +1,18 @@
-import { addValue } from '.'
 import type { AddMutation, RemoveMutation } from '..'
 import type { ValueObject } from '../../../chart'
-import { removeFromOrdered } from '../../../utils/ordered'
+import { addToOrdered, removeFromOrdered } from '../../../utils/ordered'
 import { toBpmEntity, type BpmEntity } from '../../entities/values/bpm'
 import { toBpmIntegral } from '../../integrals/bpms'
-import { removeFromStoreGrid } from '../../store/grid'
+import { addToStoreGrid, removeFromStoreGrid } from '../../store/grid'
 
-export const addBpm: AddMutation<ValueObject> = ({ store, bpms }, object) =>
-    addValue(store, bpms, object, toBpmIntegral, toBpmEntity)
+export const addBpm: AddMutation<ValueObject> = ({ store, bpms }, object) => {
+    addToOrdered(bpms, 'x', toBpmIntegral(object))
+
+    const entity = toBpmEntity(object)
+    addToStoreGrid(store.grid, entity, entity.beat)
+
+    return [entity]
+}
 
 export const removeBpm: RemoveMutation<BpmEntity> = ({ store, bpms }, entity) => {
     removeFromOrdered(bpms, 'x', entity.beat)
--- src/state/mutations/values/timeScale.ts
+++ src/state/mutations/values/timeScale.ts
@@ -1,13 +1,18 @@
-import { addValue } from '.'
 import type { AddMutation, RemoveMutation } from '..'
 import type { ValueObject } from '../../../chart'
-import { removeFromOrdered } from '../../../utils/ordered'
+import { addToOrdered, removeFromOrdered } from '../../../utils/ordered'
 import { toTimeScaleEntity, type TimeScaleEntity } from '../../entities/values/timeScale'
 import { toTimeScaleIntegral } from '../../integrals/timeScales'
-import { removeFromStoreGrid } from '../../store/grid'
+import { addToStoreGrid, removeFromStoreGrid } from '../../store/grid'
 
-export const addTimeScale: AddMutation<ValueObject> = ({ store, timeScales }, object) =>
-    addValue(store, timeScales, object, toTimeScaleIntegral, toTimeScaleEntity)
+export const addTimeScale: AddMutation<ValueObject> = ({ store, timeScales }, object) => {
+    addToOrdered(timeScales, 'beat', toTimeScaleIntegral(object))
+
+    const entity = toTimeScaleEntity(object)
+    addToStoreGrid(store.grid, entity, entity.beat)
+
+    return [entity]
+}
 
 export const removeTimeScale: RemoveMutation<TimeScaleEntity> = ({ store, timeScales }, entity) => {
     removeFromOrdered(timeScales, 'beat', entity.beat)
