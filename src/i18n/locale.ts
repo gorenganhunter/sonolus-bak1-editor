0fb2ba9ef5bd00e7fd3dfb963937ab5ced462e22
Burrito <burritoyangster@gmail.com>
2025-07-27 22:42:38 +0800

    Fix missing locale detection (#50)

--- src/i18n/locale.ts
+++ src/i18n/locale.ts
@@ -3,8 +3,10 @@ export const defaultLocale = (() => {
 
     // eslint-disable-next-line @typescript-eslint/switch-exhaustiveness-check
     switch (main) {
+        case 'fr':
         case 'ja':
         case 'ko':
+        case 'pt':
             return main
         default:
             return 'en'
